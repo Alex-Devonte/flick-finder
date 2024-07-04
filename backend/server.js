2178ca54e552a4a41aceb115c419cb0807d7adef
Alex <alex95.taylor@gmail.com>
2024-07-04 18:00:48 -0400

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { createHandler } from "graphql-http";
+import { buildSchema } from "graphql";
+import { ruruHTML } from "ruru/server";
+
+const app = express();
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello() {
+    return "Hello world!";
+  },
+};
+
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+// Start the server at port
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl">Flick Finder</h1>
+    </>
+  );
+}
+
+export default App;
