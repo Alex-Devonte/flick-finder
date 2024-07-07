544d79feaa9cbf0066994aee59d48b9507d5e648
Alex <alex95.taylor@gmail.com>
2024-07-07 11:46:23 -0400

    Add custom color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        charcoal: "#2d2d2d", // Softer black for backgrounds or text
+        "vibrant-red": "#e11d48", // Vibrant red
+        "dark-red": "#9f1239", // Darker shade of red for text or accents
+        "neutral-gray": "#6b7280", // Neutral gray
+        cream: "#ffedd5", // Base cream color
+        "dark-cream": "#fcdcb2", // Darker cream for accents
+        "light-cream": "#fff7eb", // Lighter cream for backgrounds or highlights
+      },
+    },
   },
   plugins: [],
 };
