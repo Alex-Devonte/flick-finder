3cdd7628bea42d38f14f1815a6c6ac922f9b7631
Alex <alex95.taylor@gmail.com>
2024-07-21 18:52:30 -0400

    Update schema with actual API fields
    Implement axios for making call to API

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,64 @@
 import express from "express";
-import { createHandler } from "graphql-http";
+import { createHandler } from "graphql-http/lib/use/express";
 import { buildSchema } from "graphql";
 import { ruruHTML } from "ruru/server";
+import axios from "axios";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
+    search(query: String!): [Results]
+  }
+
+  type Results {
+    id: Int
+    name: String
+    title: String
+    poster_path: String
+    media_type: String
+    release_date: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello() {
-    return "Hello world!";
+  search: async ({ query }) => {
+    const options = {
+      method: "GET",
+      url: `${process.env.TMDB_BASE_URL}query=${query}`,
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMBD_API_Token}`,
+      },
+    };
+
+    try {
+      console.log(`Searching for movies with query: ${query}`);
+      const response = await axios.request(options);
+      console.log(response);
+      console.log(
+        `Response from TMDB: ${JSON.stringify(response.data.results)}`
+      );
+
+      return response.data.results.map((result) => ({
+        id: result.id,
+        title: result.title, //Movies
+        name: result.name, //Actors & Tv Shows
+        poster_path: result.poster_path,
+        media_type: result.media_type,
+        release_date: result.release_date,
+      }));
+    } catch (error) {
+      console.error("Error fetching data from TMDB:", error);
+      throw new Error("Failed to fetch data from TMDB");
+    }
   },
 };
+
+const app = express();
 
 // Create and use the GraphQL handler.
 app.all(
