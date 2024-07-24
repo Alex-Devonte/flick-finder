2cba3a0c52b673b154f77d95add06da1a6c53ca4
Alex <alex95.taylor@gmail.com>
2024-07-24 18:17:33 -0400

    Add component for the searchbar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import Icon from "@mdi/react";
+import { mdiMagnify } from "@mdi/js";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const handleSearch = (e) => {
+    navigate("/results");
+  };
+
+  return (
+    <div className="flex w-full rounded-lg outline-dark-cream focus-within:outline md:w-[80%] lg:w-[45%]">
+      <input
+        type="search"
+        placeholder="Search for a movie or actor. . ."
+        className="flex-1 rounded-l-lg border-none p-3 text-charcoal outline-none"
+        value={query}
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        className="rounded-r-lg bg-dark-cream p-3 hover:bg-light-cream"
+        onClick={handleSearch}
+      >
+        <Icon path={mdiMagnify} size={1} className="text-charcoal" />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
