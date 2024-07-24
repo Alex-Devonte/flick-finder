5d77b897c0b51767dce509999ffdac7cbc9a05b0
Alex <alex95.taylor@gmail.com>
2024-07-24 18:19:29 -0400

    Replace searchbar markup with the SearchBar component

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -5,7 +5,8 @@ function Homepage() {
     <div className="relative flex-grow bg-[url('/cinemabackdropcropped.jpg')] bg-cover bg-center p-5 sm:bg-[url('/cinemabackdropcropped.jpg')] md:bg-[url('/cinemabackdrop.jpg')]">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-80"></div>
-      <div className="relative z-10 flex h-full flex-col items-center justify-center">
+
+      <div className="relative z-10 flex flex-col items-center justify-center">
         <h1 className="my-20 text-6xl font-bold text-cream lg:text-8xl">
           Welcome to Flick Finder
         </h1>
