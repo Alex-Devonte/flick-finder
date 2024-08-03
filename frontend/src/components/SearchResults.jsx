82216e1f1e94009c5d5a839b79d324a9bbe2d88d
Alex <alex95.taylor@gmail.com>
2024-08-02 21:39:05 -0400

    Update mobile styles

--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -1,9 +1,30 @@
 import { useSearchParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 function SearchResults() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
+
+  const MediaResult = ({ result }) => (
+    <>
+      <div className="flex-shrink-0">
+        <img
+          src={
+            result.poster_path
+              ? `${import.meta.env.VITE_IMAGE_BASE_URL}${result.poster_path}`
+              : import.meta.env.VITE_PLACEHOLDER_URL
+          }
+          alt={`${result.title || result.name} Poster`}
+          className="mr-5 h-40 object-cover"
+        />
+      </div>
+      <div>
+        <Link className="font-bold">{result.name || result.title}</Link>
+        <p className="mb-4 text-left italic">{result.release_date}</p>
+      </div>
+    </>
+  );
 
   const SEARCH_QUERY = gql`
     query Search($query: String!) {
@@ -39,42 +60,51 @@ function SearchResults() {
   console.log(groupedResults);
 
   return (
-    <>
+    <div className="p-5">
       {(groupedResults.movie || groupedResults.tv) && (
         <div>
-          <h1 className="text-7xl">Movies & TV</h1>
-          {(groupedResults.movie || []).map((result) => (
-            <div key={result.id} className="border-2 border-dark-red bg-cream">
-              <p>Title: {result.title}</p>
-              <p>Poster Path: {result.poster_path}</p>
-              <p>Release Date: {result.release_date}</p>
-              <p>Media Type: {result.media_type}</p>
-            </div>
-          ))}
-          {(groupedResults.tv || []).map((result) => (
-            <div key={result.id} className="border-2 border-dark-red bg-cream">
-              <p>Name: {result.name}</p>
-              <p>Poster Path: {result.poster_path}</p>
-              <p>Release Date: {result.release_date}</p>
-              <p>Media Type: {result.media_type}</p>
-            </div>
-          ))}
+          <h1 className="mb-6 text-7xl">Movies & TV</h1>
+          <ul className="md:grid-col-2 grid gap-4 lg:grid-cols-4">
+            {(groupedResults.movie || []).map((result) => (
+              <li key={result.id} className="flex items-center border-b p-4">
+                <MediaResult result={result} />
+              </li>
+            ))}
+            {(groupedResults.tv || []).map((result) => (
+              <li key={result.id} className="flex items-center border-b p-4">
+                <MediaResult result={result} />
+              </li>
+            ))}
+          </ul>
         </div>
       )}
 
       {groupedResults.person && (
         <div>
-          <h1 className="text-7xl">People</h1>
-          {groupedResults.person.map((result) => (
-            <div key={result.id} className="border-2 border-dark-red bg-cream">
-              <p>Name: {result.name}</p>
-              <p>Profile Path: {result.profile_path}</p>
-              <p>Media Type: {result.media_type}</p>
-            </div>
-          ))}
+          <h1 className="mb-6 text-7xl">People</h1>
+          <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            {groupedResults.person.map((result) => (
+              <li key={result.id} className="flex items-center border-b p-4">
+                <div>
+                  <img
+                    src={
+                      result.profile_path
+                        ? `${import.meta.env.VITE_IMAGE_BASE_URL}${result.profile_path}`
+                        : import.meta.env.VITE_PLACEHOLDER_URL
+                    }
+                    alt={`${result.name} Photo`}
+                    className="mr-5 h-20 w-20 rounded-full object-cover"
+                  />
+                </div>
+                <div>
+                  <Link className="text-2xl font-bold">{result.name}</Link>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
