d7d715c5f0cef9e590d9b05ea272d604800c4860
Alex <alex95.taylor@gmail.com>
2024-08-04 18:10:00 -0400

    Add routes for actor & media detail pages

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Homepage from "./components/Homepage";
 import SearchResults from "./components/SearchResults";
+import MediaDetail from "./components/MediaDetail";
+import ActorDetail from "./components/ActorDetail";
 
 const AppRouter = () => {
   return (
@@ -10,6 +12,8 @@ const AppRouter = () => {
         <Route path="/" element={<App />}>
           <Route index element={<Homepage />} />
           <Route path="results" element={<SearchResults />} />
+          <Route path="/details/media/:id" element={<MediaDetail />} />
+          <Route path="/details/people/:id" element={<ActorDetail />} />
         </Route>
       </Routes>
     </Router>
