4144bfec24a8a031c84c2277c4ac3e9f7c5af683
Alex <alex95.taylor@gmail.com>
2024-08-18 19:22:58 -0400

    Update queries with the addition fields & display all info

--- frontend/src/components/MediaDetail.jsx
+++ frontend/src/components/MediaDetail.jsx
@@ -14,8 +14,13 @@ function MediaDetail() {
         id
         title
         overview
+        backdrop_path
         poster_path
         release_date
+        genres {
+          id
+          name
+        }
       }
     }
   `;
@@ -26,7 +31,13 @@ function MediaDetail() {
         id
         name
         overview
+        first_air_date
         backdrop_path
+        poster_path
+        genres {
+          id
+          name
+        }
       }
     }
   `;
@@ -38,7 +49,33 @@ function MediaDetail() {
     },
   );
 
-  console.log(data);
+  //Format data into a more manageable object to make it easier to display properties
+  const formatData = (data, mediaType) => {
+    if (!data) return null;
+
+    if (mediaType === "movie") {
+      return {
+        title: data.movie.title,
+        poster_path: data.movie.poster_path,
+        backdrop_path: data.movie.backdrop_path,
+        overview: data.movie.overview,
+        release_date: data.movie.release_date,
+        genres: data.movie.genres,
+      };
+    } else {
+      return {
+        title: data.tv.name,
+        poster_path: data.tv.poster_path,
+        backdrop_path: data.tv.backdrop_path,
+        overview: data.tv.overview,
+        release_date: data.tv.first_air_date,
+        genres: data.tv.genres,
+      };
+    }
+  };
+
+  const mediaData = formatData(data, mediaType);
+  console.log(mediaData);
 
   if (loading) return <p>Loading . . . </p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -46,7 +83,26 @@ function MediaDetail() {
   return (
     <>
       <p>Viewing detail for: {id}</p>
-      <p>{data.movie?.title || data.tv?.name}</p>
+      <img
+        src={`${import.meta.env.VITE_IMAGE_BASE_URL}${mediaData.poster_path}`}
+        alt={`${mediaData.title} Poster`}
+      />
+      <img
+        src={`${import.meta.env.VITE_IMAGE_BASE_URL}${mediaData.backdrop_path}`}
+        alt={`${mediaData.title} Backdrop`}
+      />
+      <p>{mediaData.title}</p>
+      <p>Overview: {mediaData.overview}</p>
+      <p>
+        {mediaType === "movie" ? "Release Date:" : "First Air Date:"}{" "}
+        {mediaData.release_date}
+      </p>
+      <p>Genres:</p>
+      <ul>
+        {mediaData.genres?.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
