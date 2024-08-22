e1e803c6e980ea437b145069870df41fa26a26d7
Alex <alex95.taylor@gmail.com>
2024-08-22 17:21:47 -0400

    Implement actor query and data retrieval

--- frontend/src/components/ActorDetail.jsx
+++ frontend/src/components/ActorDetail.jsx
@@ -1,8 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 
 function ActorDetail() {
   //Extract actor id from the URL
   const { id } = useParams();
+  const actorID = parseInt(id, 10);
+
+  const ACTOR_QUERY = gql`
+    query Actor($id: Int!) {
+      actor(id: $id) {
+        id
+        biography
+        birthday
+        deathday
+        gender
+        known_for_department
+        name
+        place_of_birth
+        profile_path
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(ACTOR_QUERY, {
+    variables: { id: actorID },
+  });
+
+  console.log(data);
+
+  if (loading) return <p>Loading . . .</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return <p>Viewing detail for: {id}</p>;
 }
 
