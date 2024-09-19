21ba4ab3971ab1628b95cd002acff4600bb8d1d5
Alex <alex95.taylor@gmail.com>
2024-09-19 17:09:56 -0400

    Implement general layout of information
    -Implement logic for formatting crew data, converting
     runtime, and display crew separated by role.

--- frontend/src/components/MediaDetail.jsx
+++ frontend/src/components/MediaDetail.jsx
@@ -17,6 +17,9 @@ function MediaDetail() {
         backdrop_path
         poster_path
         release_date
+        rating
+        tagline
+        runtime
         genres {
           id
           name
@@ -27,6 +30,11 @@ function MediaDetail() {
             name
             profile_path
             character
+          }
+          crew {
+            id
+            name
+            role
           }
         }
       }
@@ -42,6 +50,7 @@ function MediaDetail() {
         first_air_date
         backdrop_path
         poster_path
+        rating
         genres {
           id
           name
@@ -72,20 +81,35 @@ function MediaDetail() {
 
   console.log("RAW DATA: ", data);
 
-  //Format data into a more manageable object to make it easier to display properties
-  const formatData = (data, mediaType) => {
+  //Format media data into a more manageable object to make it easier to display properties
+  const formatMediaData = (data, mediaType) => {
     if (!data) return null;
 
     if (mediaType === "movie") {
-      return {
-        title: data.movie.title,
-        poster_path: data.movie.poster_path,
-        backdrop_path: data.movie.backdrop_path,
-        overview: data.movie.overview,
-        release_date: data.movie.release_date,
-        genres: data.movie.genres,
-        credits: data.movie.credits,
+      //Destructure credits and movie data
+      const { credits, ...movie } = data.movie;
+
+      const groupedCrew = {};
+      credits.crew.forEach((person) => {
+        if (!groupedCrew[person.role]) {
+          groupedCrew[person.role] = [];
+        }
+        groupedCrew[person.role].push(person);
+      });
+
+      console.log(groupedCrew);
+
+      const mediaData = {
+        ...movie,
+        credits: {
+          cast: credits.cast,
+          crew: groupedCrew,
+        },
       };
+
+      console.log(mediaData);
+
+      return mediaData;
     } else {
       return {
         title: data.tv.name,
@@ -93,42 +117,128 @@ function MediaDetail() {
         backdrop_path: data.tv.backdrop_path,
         overview: data.tv.overview,
         release_date: data.tv.first_air_date,
+        rating: data.tv.rating,
         genres: data.tv.genres,
         credits: data.tv.credits,
       };
     }
   };
 
-  const mediaData = formatData(data, mediaType);
+  const convertRuntime = (runtime) => {
+    const hours = Math.floor(runtime / 60);
+    const remainingMinutes = runtime % 60;
+    return `${hours}h ${remainingMinutes}m`;
+  };
+
+  const mediaData = formatMediaData(data, mediaType);
+
   console.log(mediaData);
+
+  const displayCrew = (mediaData) => {
+    const crew = mediaData.credits.crew;
+    console.log(crew);
+
+    const directors = crew.Director.filter(
+      (member) => member.role === "Director",
+    );
+
+    const writers = crew.Writer.filter((member) => member.role === "Writer");
+
+    return (
+      <div>
+        <ul className="">
+          {directors.length > 0 && (
+            <li>
+              <span className="font-bold">
+                Director{directors.length > 1 ? "s" : ""}:{" "}
+              </span>
+              <ul className="inline p-2">
+                {directors.map((director) => (
+                  <li className="inline" key={director.id}>
+                    {director.name}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )}
+          {writers.length > 0 && (
+            <li>
+              <span className="font-bold">
+                Writer{writers.length > 1 ? "s" : ""}:{" "}
+              </span>
+              <ul className="inline p-2">
+                {writers.map((writer) => (
+                  <li className="inline p-1" key={writer.id}>
+                    {writer.name}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  };
 
   if (loading) return <p>Loading . . . </p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <>
-      <p>Viewing detail for: {id}</p>
-      <img
-        src={`${import.meta.env.VITE_IMAGE_BASE_URL}${mediaData.poster_path}`}
-        alt={`${mediaData.title} Poster`}
-      />
-      <img
-        src={`${import.meta.env.VITE_IMAGE_BASE_URL}${mediaData.backdrop_path}`}
-        alt={`${mediaData.title} Backdrop`}
-      />
-      <p>{mediaData.title}</p>
-      <p>Overview: {mediaData.overview}</p>
-      <p>
-        {mediaType === "movie" ? "Release Date:" : "First Air Date:"}{" "}
-        {mediaData.release_date}
-      </p>
-      <p>Genres:</p>
-      <ul>
-        {mediaData.genres?.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
-    </>
+    <div className="relative text-left">
+      {/* Background Backdrop */}
+      <div
+        className="absolute inset-0 z-0 flex h-[300px] w-full bg-cover bg-top bg-no-repeat lg:h-[700px]"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${import.meta.env.VITE_IMAGE_BASE_URL}original${mediaData.backdrop_path})`,
+        }}
+      ></div>
+
+      {/* Poster & Movie Details */}
+      <div className="relative z-10 flex flex-col lg:h-[700px] lg:flex-row">
+        <div className="h-[300px] p-3 lg:h-full lg:w-1/3">
+          <img
+            className="h-full w-full object-contain"
+            src={`${import.meta.env.VITE_IMAGE_BASE_URL}original${mediaData.poster_path}`}
+            alt={`${mediaData.title} Poster`}
+          />
+        </div>
+
+        <div className="p-5 lg:w-2/3">
+          <h1 className="text-3xl font-bold">
+            {mediaData.title}
+            <span> ({mediaData.release_date.slice(0, 4)})</span>
+          </h1>
+          <p className="border">{mediaData.rating}</p>
+          <p>{convertRuntime(mediaData.runtime)}</p>
+          <ul>
+            {mediaData.genres.map((genre) => (
+              <li key={genre.id}>{genre.name}</li>
+            ))}
+          </ul>
+          <p className="text-black">{mediaData.tagline}</p>
+          <p className="text-left text-lg text-black">{mediaData.overview}</p>
+          {/* Crew */}
+          {displayCrew(mediaData)}
+        </div>
+      </div>
+
+      {/* Cast */}
+      <div className="relative z-20 m-2 bg-white p-4">
+        <h2 className="text-xl font-semibold">Top Billed Cast</h2>
+        <div className="flex gap-10 overflow-x-scroll">
+          {mediaData.credits.cast.map((cast) => (
+            <div key={cast.id}>
+              <img
+                className="h-60 w-60"
+                src={`${import.meta.env.VITE_IMAGE_BASE_URL}original${cast.profile_path}`}
+              />
+              <p>{cast.name}</p>
+              <p>{cast.character}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
