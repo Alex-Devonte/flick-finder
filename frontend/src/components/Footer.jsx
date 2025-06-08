0a55017abe5eb9f2b2b8e224a44187d85e169cbc
Alex <alex95.taylor@gmail.com>
2025-06-08 18:24:45 -0400

    Update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="z-10 bg-black p-5 text-center">
-      <p className="text-dark-cream">Created by Alex Taylor 2024</p>
+      <p className="text-dark-cream">Created by Alex Taylor 2025</p>
     </div>
   );
 }
